f2e44f89cd13df16ca2789ddb5b751a7ea287d35
Kitanae <72314886+kee-ta@users.noreply.github.com>
2021-10-11 12:03:16 +0900

    Added Dialogue Framework
    Unfinished dialogue class
    dialogue manager

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+
+[XmlRoot("Dialogue")]
+public class Dialogue
+{
+    
+#region Getters and Setters
+
+#endregion
+
+#region Constructors
+//Default Empty Constructor
+ public Dialogue(){
+
+ }
+ // Next here...
+ #endregion
+
+}
