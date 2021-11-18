004f3ff0d6b9916b7d27191e9c90977e8d076a42
lianqi541 <holianqi@gmail.com>
2021-11-18 14:45:15 +0800

    Squashed commit of the following:
    commit 8e47959dc95f32bb39794c4d9385fc5de8427f81
    Author: lianqi541 <holianqi@gmail.com>
    Date:   Tue Nov 16 23:19:10 2021 +0800
    
        MainMenuScene
    
        [Sprites, Font, BG etc can be changed]
        Created a Main Menu Scene with 3 buttons:
        - Play (Allows player to navigate to the game scene)
        - Instructions (Allows player to learn how to play) [Needs Update]
        - Quit (Quits the program)
    
    commit 9684da92ede42c39beb5e42c4b343fc818af82e9
    Author: lianqi541 <holianqi@gmail.com>
    Date:   Mon Nov 8 16:20:43 2021 +0800
    
        Dialogue System
    
        - Left with the art/content and animation of dialogue box.
        - Already included the typing text effect.
        - Can add/remove edit the conversation and name of different NPC easily.

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+    
+    //Keeps track of all the sentences in our current dialogue
+    private Queue<string> sentences; 
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("Is Open", true);
+        
+        nameText.text = dialogue.name;
+
+        sentences.Clear(); // Clears all the sentences from the previous conversations
+
+        foreach(string sentence in dialogue.sentences) // Loops through all the sentences in our dialogue and display them
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence() // End conversation if no more sentences left to say
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue(); // Sentences left to say
+        StopAllCoroutines(); // Make sure TypeSentence is stopped 
+        StartCoroutine(TypeSentence(sentence));
+    }
+    
+    IEnumerator TypeSentence(string sentence) // Automatically type sentences 
+    {
+        dialogueText.text = "";
+
+        // Loops through all the single characters in sentence
+        foreach (char letter in sentence.ToCharArray()) // ToCharArray() converts string to character array
+        {
+            dialogueText.text += letter;
+            yield return null; 
+        }
+
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("Is Open", false);
+    }
+
+}
