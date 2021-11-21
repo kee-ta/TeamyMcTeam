053209bfa1244b9f6d1f17b0b87f3f7908727139
Kitanae <72314886+kee-ta@users.noreply.github.com>
2021-11-21 16:22:26 +0900

    Added npc shops to town scene

---
+++ Assets/Scripts/NPC Shop/Different NPC Shops/ShopTriggerCollider1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTriggerCollider1 : MonoBehaviour {
+
+    [SerializeField] private UI_Shop_1 uiShop;
+
+    private void SetUpShop(){
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        Debug.Log("Entered Shop Area");
+        IShopCustomer shopCustomer = collider.GetComponent<IShopCustomer>();
+        if (shopCustomer != null) {
+            Debug.Log("Showing Shop");
+            uiShop.Show(shopCustomer);
+            SetUpShop();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+         Debug.Log("Left Shop Area");
+        IShopCustomer shopCustomer = collider.GetComponent<IShopCustomer>();
+        if (shopCustomer != null) {
+            uiShop.Hide();
+        }
+    }
+
+}
