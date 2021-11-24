e5114dc067bdd570627b4b2ca93486a715d6ecbb
yiipy <yipxiuhan@gmail.com>
2021-11-24 13:46:08 +0800

    Fixed Player Shop Show Bug

--- Assets/ShopSystem/PlayerShopTriggerCollider.cs
+++ Assets/ShopSystem/PlayerShopTriggerCollider.cs
@@ -6,36 +6,46 @@ public class PlayerShopTriggerCollider : MonoBehaviour
 {
     public GameObject PlayerShopCanvas;
 
-    private IShopCustomer shopCustomer1;
 
+    [SerializeField] private UI_Shop_2 uiShop;
+
+    private IShopCustomer shopCustomer1;
+    private bool canShow = false;
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("Entered Shop Area");
-
+        canShow = true;
         IShopCustomer shopCustomer = collider.GetComponent<IShopCustomer>();
         shopCustomer1 = shopCustomer;
-
         if (shopCustomer != null)
         {
-            PlayerController.instance.PromptActivate();
+            PlayerController.instance.PromptDialogueActivate();
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.C) && canShow)
         {
             PlayerShopCanvas.SetActive(true);
+            PlayerController.instance.PromptDialogueDeactivate();
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && canShow)
+        {
+            PlayerController.instance.PromptDialogueDeactivate();
+            gameObject.GetComponent<DialogueTrigger>().TriggerDialogue(); // Triggers dialogue that this gameObject is attached to
+            PlayerController.instance.PromptActivate();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        Debug.Log("Left Player Shop Area");
-
+        Debug.Log("Left Shop Area");
+        canShow = false;
         IShopCustomer shopCustomer = collider.GetComponent<IShopCustomer>();
         if (shopCustomer != null)
         {
+            PlayerController.instance.PromptDialogueDeactivate();
             PlayerController.instance.PromptDeactivate();
             PlayerShopCanvas.SetActive(false);
         }
