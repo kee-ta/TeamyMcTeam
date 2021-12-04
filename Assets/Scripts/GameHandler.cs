8b59a9e082371ace233703a5fcc821c68671a902
Kitanae <72314886+kee-ta@users.noreply.github.com>
2021-12-04 21:02:43 +0900

    Changes to NPC Shop and Overworld UI
    Changed NPC shops to close on keypress
    Changed Overworld UI to use new sprites
    Changed NPC Shop to use new sprites

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -4,9 +4,40 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
     private void Awake(){
         SoundManager.Initialize();
+=======
+    public TextMeshProUGUI moneyCount;
+    public GameObject playerInventoryUI;
+    public int playerMoney = 10;
+    public TextMeshProUGUI stickQtn;
+    public TextMeshProUGUI leafQtn;
+    public TextMeshProUGUI flowerQtn;
+    public TextMeshProUGUI stoneQtn;
+    public void addMoney(int moneyToAdd){
+        playerMoney += moneyToAdd;
+    }
+    private void Awake(){
+        SoundManager.Initialize();
+        playerMoney =  PlayerController.instance.GetGoldAmount();   
+ 
+    }
+
+    public void updateMoney(){
+        playerMoney =  PlayerController.instance.GetGoldAmount(); 
+        moneyCount.text = playerMoney.ToString();
+    }
+
+    public void ShowInventory(){
+        
+        playerInventoryUI.SetActive(true);
+        stickQtn.text = PlayerController.instance.GetItemAmount(Item.ItemType.Stick).ToString();
+        leafQtn.text = PlayerController.instance.GetItemAmount(Item.ItemType.Leaf).ToString();
+        flowerQtn.text = PlayerController.instance.GetItemAmount(Item.ItemType.Flower).ToString();
+        stoneQtn.text = PlayerController.instance.GetItemAmount(Item.ItemType.Stone).ToString();
+>>>>>>> Stashed changes
     }
     // Start is called before the first frame update
     void Start()
@@ -17,6 +48,11 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
+=======
+        ShowInventory();
+        updateMoney();
+>>>>>>> Stashed changes
     }
 }
