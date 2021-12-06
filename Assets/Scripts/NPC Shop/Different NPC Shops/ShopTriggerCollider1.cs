228b29a98b8b3e4d2359e30c463e80e2ee846297
Kitanae <72314886+kee-ta@users.noreply.github.com>
2021-12-06 11:52:49 +0900

    Updated Tutorial and ShopColliders
    Added functionality to check for player within collision box of npc before continuing with tutorial

--- Assets/Scripts/NPC Shop/Different NPC Shops/ShopTriggerCollider1.cs
+++ Assets/Scripts/NPC Shop/Different NPC Shops/ShopTriggerCollider1.cs
@@ -39,6 +39,9 @@ public class ShopTriggerCollider1 : MonoBehaviour {
         }
     }
 
+    public bool HasCollision(){
+        return canShow;
+    }
 
     private void OnTriggerExit2D(Collider2D collider) {
         Debug.Log("Left Shop Area");
